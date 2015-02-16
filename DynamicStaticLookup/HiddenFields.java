67313f0efea7c3175db9e54ab0d8a80ef13b2d7a
Alvin Wan <hi@alvinwan.com>
2015-02-16 15:02:37 -0800

    added 'hidden public variable' to HiddenFields

--- DynamicStaticLookup/HiddenFields.java
+++ DynamicStaticLookup/HiddenFields.java
@@ -1,7 +1,6 @@
 /**
  * HIDDEN VARIABLES
- * This file demonstrates how variables are hidden
- * (or not). 
+ * This file demonstrates how variables are hidden (or not).
  * 
  * @author Alvin Wan
  * @website alvinwan.com
@@ -24,13 +23,20 @@ public class HiddenFields {
 		print(spaceship.name); // normal behavior - prints "Birdie"
 		
 		SpaceShip spaceship2 = new SpaceShip(); // initialized without name for SpaceShip
-		
+
 		print(spaceship2.name); // name is not defined for the SpaceShip context (returns null)
 		spaceship2.getName(); // getName() defined in Ship, which has name of "Catalina"
 
 		/**
-		 * 
+		 * This is a strange form of abstraction, which
+		 * could really be achieved with a private variable.
+		 * However, for demonstration purposes, the classes
+		 * below "hide" variables using public declarations.
 		 */
+		
+		SpaceShip mayday = new SpaceShip(true); // have parent class Ship set isDamaged variable
+		mayday.isDamaged(); // calls super, which has access to the isDamaged variable -> true
+		mayday.isDamagedFaulty(); // attempts to find isDamaged in SpaceShip -> false (default value for booleans)
 	}
 	
 	public static void print(String msg) {
@@ -40,9 +46,18 @@ public class HiddenFields {
 
 class Ship {
 	public String name;
+	public boolean isDamaged;
 	
 	public Ship() {
 		name = "Catalina";
+	}
+	
+	public Ship(boolean _isDamaged) {
+		isDamaged = _isDamaged;
+	}
+	
+	public void isDamaged() {
+		System.out.println(isDamaged);
 	}
 	
 	public void fly() {
@@ -57,6 +72,7 @@ class Ship {
 class SpaceShip extends Ship {
 	public String name;
 	public boolean isFlying;
+	public boolean isDamaged;
 	
 	public SpaceShip() {
 		super();
@@ -67,6 +83,18 @@ class SpaceShip extends Ship {
 		super();
 		name = _name;
 		isFlying = false;
+	}
+	
+	public SpaceShip(boolean _isDamaged) {
+		super(_isDamaged);
+	}
+	
+	public void isDamaged() {
+		super.isDamaged();
+	}
+	
+	public void isDamagedFaulty() {
+		System.out.println(isDamaged);
 	}
 	
 	public void fly() {
