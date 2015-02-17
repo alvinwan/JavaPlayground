16779ab29457220ba1ea08d6df25627a97fa3fa2
Alvin Wan <hi@alvinwan.com>
2015-02-17 15:15:09 -0800

    structure for Override and Overload file

---
+++ Concepts/Inheritance/OverrideAndOverload.java
@@ -0,0 +1,15 @@
+/**
+ * OVERRIDE & OVERLOAD
+ * This file demonstrates the utility of overriding and
+ * overloading. Both over's are used fairly profilically across
+ * files in the JavaPlayground.
+ *
+ * @author Alvin Wan
+ * @website alvinwan.com
+ */
+
+public class OverrideAndOverload {
+    public static void main(String[] args) {
+
+    }
+}
